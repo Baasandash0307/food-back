520796b9ddd69988da5ced979c308ec0d20c0337
24LU4966 24LU4966 <bsskaa.kai123@gmail.com>
2025-04-04 12:32:34 +0800

    new

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 import dotenv from 'dotenv'
 dotenv.config()
 
-const MONGODB_URI = process.env.MONGODB_URI as string
+const MONGODB_URI = process.env.MONGODB_URI || ''
 
 export const connection = async () => {
     await mongoose.connect(MONGODB_URI)
